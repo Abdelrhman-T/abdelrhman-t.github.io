94f207e61f98490c0ad701d3fb71ef1e378a17b3
Abdelrhman-T <abdelrhmantarek2002@gmail.com>
2026-06-22 22:45:10 +0300

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/abdelrhman-t.github.io/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
